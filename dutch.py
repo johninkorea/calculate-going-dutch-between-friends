a844f1e984dd15f56396a491e5e7bf28e5f3713b
johninkorea <john829829@gmail.com>
2021-01-16 09:11:58 +0900

    fix introduction &add announcement

--- dutch.py
+++ dutch.py
@@ -1,3 +1,6 @@
-# This things be made by Wonseok Ryu (git hub id: johninkorea)
+# This file was created by Wonseok Ryu (git hub id: johninkorea)
 
+# announcement
+print("여러명의 더치 페이 계산을 도와줍니다.")
+print('금액을 입력할때 수학 기호를 포함하여도 계산해 드립니다')
 
